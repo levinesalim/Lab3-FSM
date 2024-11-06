9ab9406908e88750ed3bf1554f5ab2df9229d6b4
levinesalim <levinesalim05@gmail.com>
2024-11-06 08:57:39 +0000

    Add files via upload

--- task1/lfsr.sv
+++ task1/lfsr.sv
@@ -1,8 +1,21 @@
+//we will include everything from the design
+//we must have a signal sine generation
+//vbdFlag() is needed
 module lfsr(
     input   logic       clk,
     input   logic       rst,
     input   logic       en,
-    output  logic [3:0] data_out
+    output  logic [4:1] data_out
 );
 
+    logic[4:1] sreg; //adding definition for sreg
+
+always_ff @ (posedge clk, posedge rst)
+    if (rst)
+        sreg <= 4'b1;
+    else begin
+        sreg <= {sreg[3:1], sreg[4] ^ sreg[3]};
+    end
+
+assign data_out = sreg; 
 endmodule
